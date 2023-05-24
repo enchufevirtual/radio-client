5e0742a3f83cb1275e72d3c2da34f0f2d9adc61f
chendo <chendoec@live.com>
2023-05-24 13:50:10 -0500

    Updated redirects

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,8 @@ module.exports = (env) => {
     output: {
       path: path.resolve(__dirname, 'dist'),
       filename: 'index.js',
-      clean: true
+      clean: true,
+      publicPath: '/'
     },
     mode: 'development',
     resolve: {
